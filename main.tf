c136582e69e5fdd04c052085fa3eb7e546a741b9
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-08-19 18:03:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 # create RDS db instance
-terraform {
-  experiments = [provider_sensitive_attrs]
-}
+
 resource "aws_db_instance" "this" {
   count                   = var.enabled ? 1 : 0
   allocated_storage       = var.allocated_storage
@@ -14,7 +12,7 @@ resource "aws_db_instance" "this" {
   identifier              = var.identifier
   instance_class          = var.instance_class
   username                = local.sso_secrets.username
-  password                = sensitive(local.sso_secrets.password)
+  password                = local.sso_secrets.password
   skip_final_snapshot     = var.skip_final_snapshot
   storage_encrypted       = var.storage_encrypted
   vpc_security_group_ids  = var.vpc_security_group_ids
