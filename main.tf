9eabe01c9a300447c5e7ea848d06353bf6df8487
George Holbert <gholbert@ucop.edu>
2023-04-24 15:42:15 -0700

    added blue_green_update variable

--- main.tf
+++ main.tf
@@ -1,31 +1,32 @@
 # create RDS db instance
 
 resource "aws_db_instance" "this" {
-  count                   = var.enabled ? 1 : 0
-  allocated_storage       = var.allocated_storage
+  count                           = var.enabled ? 1 : 0
+  allocated_storage               = var.allocated_storage
 /* #   max_allocated_storage   = var.max_allocated_storage */
-  backup_retention_period = var.backup_retention_period
-  deletion_protection     = var.deletion_protection
-  db_subnet_group_name    = aws_db_subnet_group.this.*.id[0]
-  engine                  = var.engine
-  engine_version          = var.engine_version
-  identifier              = var.identifier
-  instance_class          = var.instance_class
-  multi_az                = var.multi_az
-  username                = local.sso_secrets.username
-  password                = local.sso_secrets.password
-  skip_final_snapshot     = var.skip_final_snapshot
-  copy_tags_to_snapshot   = var.copy_tags_to_snapshot
-  storage_encrypted       = var.storage_encrypted
-  storage_type            = var.storage_type
-  snapshot_identifier     = var.snapshot_identifier
-  vpc_security_group_ids  = var.vpc_security_group_ids
-  publicly_accessible     = var.publicly_accessible
-  parameter_group_name    = var.parameter_group_name
-  option_group_name       = var.option_group_name
-  apply_immediately       = var.apply_immediately
+  backup_retention_period         = var.backup_retention_period
+  blue_green_update               = var.blue_green_update
+  deletion_protection             = var.deletion_protection
+  db_subnet_group_name            = aws_db_subnet_group.this.*.id[0]
+  engine                          = var.engine
+  engine_version                  = var.engine_version
+  identifier                      = var.identifier
+  instance_class                  = var.instance_class
+  multi_az                        = var.multi_az
+  username                        = local.sso_secrets.username
+  password                        = local.sso_secrets.password
+  skip_final_snapshot             = var.skip_final_snapshot
+  copy_tags_to_snapshot           = var.copy_tags_to_snapshot
+  storage_encrypted               = var.storage_encrypted
+  storage_type                    = var.storage_type
+  snapshot_identifier             = var.snapshot_identifier
+  vpc_security_group_ids          = var.vpc_security_group_ids
+  publicly_accessible             = var.publicly_accessible
+  parameter_group_name            = var.parameter_group_name
+  option_group_name               = var.option_group_name
+  apply_immediately               = var.apply_immediately
   enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
-  tags                    = var.tags
+  tags                            = var.tags
 
   depends_on = [
     aws_db_subnet_group.this,
